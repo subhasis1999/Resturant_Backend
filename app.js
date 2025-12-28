1eeadd4b22f4fcd58e841a3c6514212a971c3b04
subhasis1999 <subhasissahoo6866@gmail.com>
2025-12-28 14:41:50 +0530

    Initial vercel json

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import { dbConnection } from "../backend/database/dbconnection.js"
-
-
-// const app = express();
-
-// dotenv.config({ path: './config/config.env' })
-
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["post"],
-//     credentials: [true]
-// }))
-// app.use(express.json());
-// epp.use(express.urlencoded({ extended: true }));
-// dbConnection();
-// export default app;
 
 import express from "express";
 import cors from "cors";
